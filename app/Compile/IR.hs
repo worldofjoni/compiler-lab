1828d971b2ca56f380e1c089a12534c35fb35a78
Dominik Rosch <dominik.rosch@student.kit.edu>
2025-05-23 16:18:43 +0200

    add goto to ir

--- app/Compile/IR.hs
+++ app/Compile/IR.hs
@@ -9,14 +9,22 @@ where
 import Compile.AST (Op, UnOp)
 import GHC.Arr (Array)
 
-
 type VRegister = Int -- virtual register
 
-type IR = Array Int IStmt
+type LineNo = Int
+
+type IR = Array LineNo IStmt
 
 data Operand = Reg VRegister | Imm Integer
 
-data IStmt = Return Operand | VRegister :<- Operand | VRegister :<-+ (Operand, Op, Operand) | Unary VRegister UnOp Operand
+data IStmt
+  = Return Operand
+  | VRegister :<- Operand
+  | VRegister :<-+ (Operand, Op, Operand)
+  | Unary VRegister UnOp Operand
+  | Goto LineNo
+  | GotoIf LineNo Operand
+  | Nop
 
 instance Show Operand where
   show (Reg r) = "vreg" ++ show r
@@ -27,3 +35,6 @@ instance Show IStmt where
   show (r :<- s) = show (Reg r) ++ " <- " ++ show s
   show (r :<-+ (s1, op, s2)) = show (Reg r) ++ " <- " ++ show s1 ++ " " ++ show op ++ " " ++ show s2
   show (Unary r op s) = show (Reg r) ++ " <- " ++ show op ++ show s
+  show Nop = "nop"
+  show (Goto l) = "goto " ++ show l
+  show (GotoIf l op) = "goto " ++ show l ++ " if " ++ show op
