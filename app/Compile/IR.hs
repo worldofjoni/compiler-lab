38ae76a39dd1bbf8544a1dd3a842535d22859db3
Dominik Rosch <dominik.rosch@student.kit.edu>
2025-07-02 13:56:44 +0200

    basic block ir

--- app/Compile/IR.hs
+++ app/Compile/IR.hs
@@ -10,7 +10,11 @@ type Label = String
 
 type FrameSizes = Map.Map Label Int
 
-type IR = [IStmt]
+type IR = [IRFunc]
+
+type IRFunc = (String, Map.Map Label IRBasicBlock)
+data BasicBlock line = BasicBlock {lines :: [line], successors :: [Label]}
+type IRBasicBlock = BasicBlock IStmt
 
 data Operand = Reg VRegister | Imm Integer
 
@@ -19,11 +23,9 @@ data IStmt
   | VRegister :<- Operand
   | VRegister :<-+ (Operand, Op, Operand)
   | Unary VRegister UnOp Operand
-  | Label Label
   | Goto Label
   | GotoIfNot Label Operand
   | CallIr (Maybe VRegister) Label [VRegister]
-  | FunctionLabel Label
   | Nop
 
 instance Show Operand where
@@ -35,10 +37,13 @@ instance Show IStmt where
   show (r :<- s) = show (Reg r) ++ " <- " ++ show s
   show (r :<-+ (s1, op, s2)) = show (Reg r) ++ " <- " ++ show s1 ++ " " ++ show op ++ " " ++ show s2
   show (Unary r op s) = show (Reg r) ++ " <- " ++ show op ++ show s
-  show (Label l) = l ++ ":"
   show Nop = "nop"
   show (Goto l) = "goto " ++ show l
   show (GotoIfNot l op) = "goto " ++ show l ++ " if not " ++ show op
   show (CallIr tgt l regs) = maybe "" ((++ " <- ") . show . Reg) tgt ++ "call " ++ l ++ "(" ++ (intercalate ", " . map show) regs ++ ")"
+<<<<<<< Updated upstream
   show (FunctionLabel label) = label ++ ":"
 
+=======
+  show (CallTail l regs) = "tail_call " ++ l ++ "(" ++ (intercalate ", " . map show) regs ++ ")"
+>>>>>>> Stashed changes
